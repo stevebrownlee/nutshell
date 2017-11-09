264c4b473150cdde4728da07cd40213eae0731a0
Steve Brownlee <chortlehoort@gmail.com>
2017-11-08 23:03:04 -0600

    Initial commit with CSS grid layout

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        watch: {
+            scripts: {
+              files: ["**/scripts/*.js", "!node_modules/**/*.js"],
+              tasks: ["eslint", "browserify", "uglify"],
+              options: {
+                spawn: false,
+              },
+            }
+        },
+        browserify: {
+            dist: {
+                files: {
+                    "build/bundle.js": ["scripts/main.js"]
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: "/*! <%= pkg.name %> <%= grunt.template.today('yyyy-mm-dd') %> */\n"
+            },
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: "build",
+                    src: "bundle.js",
+                    dest: "build",
+                    ext: ".min.js"
+                }]
+            }
+        },
+        eslint: {
+            src: [
+                "**/scripts/*.js",
+                "!node_modules/**/*.js"
+            ]
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-eslint");
+    grunt.loadNpmTasks("grunt-browserify");
+
+    // Default task(s).
+    grunt.registerTask("default", ["eslint", "browserify", "uglify", "watch"]);
+
+};
